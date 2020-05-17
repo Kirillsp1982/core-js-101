9eb70414fc6340933e491fdb1cc74447fde564ce
Kirillsp1982 <kirillsp1982@gmail.com>
2020-05-17 15:20:38 +0300

    feat: add new solutions

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -223,8 +223,10 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const parseValue = parseInt(value, 10);
+  if (Number.isInteger(parseValue)) return value;
+  return def;
 }
 
 module.exports = {
